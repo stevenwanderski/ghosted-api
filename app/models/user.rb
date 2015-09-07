3d13ec0f7c2f401b04865e5c84ec70a8c8d3dc00
Steven Wanderski <steven@popularpays.com>
2015-09-07 11:07:00 -0500

    Fixes User#reset method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
   attr_encrypted :access_token, key: ENV['USER_ENCRYPTION_KEY']
 
   has_many :repos, through: :user_repos
-  has_many :issues, through: :user_repos
-  has_many :milestones, through: :user_repos
+  has_many :issues, through: :repos
+  has_many :milestones, through: :repos
   has_many :user_repos, dependent: :destroy
 
   def reset
-    self.milestones.destroy_all
-    self.issues.destroy_all
-    self.repos.destroy_all
+    Milestone.where(id: self.milestones.pluck(:id)).destroy_all
+    Issue.where(id: self.issues.pluck(:id)).destroy_all
+    Repo.where(id: self.repos.pluck(:id)).destroy_all
     self.user_repos.destroy_all
     self.update(repos_loaded: false)
   end
